3370df4a017ee61085a19cceb76f7c871967b448
Caroline Ramos <carolbeatrice@gmail.com>
2024-03-13 11:39:24 -0300

    Criada a classe e resolvido o problema com orientação a objetos

--- Program.cs
+++ Program.cs
@@ -6,32 +6,30 @@ namespace SalarioFuncionario
     {
         static void Main(string[] args)
         {
-            //Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). Em seguida, mostrar os dados do funcionário (nome e salário líquido). Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário. A Classe para esta atividade deve conter os atributos Nome, SalarioBruto e Imposto e os Métodos CalcularSalarioLiquido() : double e AumentarSalario(): void
-            //salario bruto = sem os descontos
-            //salario liquido = depois dos descontos
+            SalarioFuncionario x = new SalarioFuncionario();
 
             Console.WriteLine("Digite o nome do funcionario: ");
-            string nomeFuncionario = Console.ReadLine();
+            x.nomeFuncionario = Console.ReadLine();
             Console.WriteLine("Digite o salário bruto: ");
-            double salarioBruto = double.Parse(Console.ReadLine());
+            x.salarioBruto = double.Parse(Console.ReadLine());
             Console.WriteLine("Digite a porcentagem de imposto: ");
-            double imposto = double.Parse(Console.ReadLine());
+            x.imposto = double.Parse(Console.ReadLine());
 
-            double salarioLiquido = salarioBruto - (salarioBruto * (imposto / 100));
+            double salarioLiquidoX = x.CalcularSalarioLiquido();
+
 
             Console.WriteLine();
-            Console.WriteLine($"Nome do funcionario: {nomeFuncionario}\nSalário líquido: R${salarioLiquido:F2}");
+            Console.WriteLine($"Nome do funcionario: {x.nomeFuncionario}\nSalário líquido: R${x.salarioLiquido:F2}");
             Console.WriteLine();
 
             Console.WriteLine("Digite a porcentagem de aumento do salario bruto: ");
-            double aumento = double.Parse(Console.ReadLine());
+            x.aumento = double.Parse(Console.ReadLine());
 
-            salarioBruto = salarioBruto + (salarioBruto * (aumento / 100));
-
-            salarioLiquido = salarioBruto - (salarioBruto * (imposto / 100));
+            x.AumentarSalario();
+            salarioLiquidoX = x.CalcularSalarioLiquido();
 
             Console.WriteLine();
-            Console.WriteLine($"Nome do funcionario : {nomeFuncionario}\nSalário Liquido: R${salarioLiquido:F2}");
+            Console.WriteLine($"Nome do funcionario : {x.nomeFuncionario}\nSalário Liquido: R${x.salarioLiquido:F2}");
 
 
         }
---
+++ SalarioFuncionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalarioFuncionario
+{
+    public class SalarioFuncionario
+    {
+        public string nomeFuncionario;
+        public double salarioBruto;
+        public double imposto;
+        public double salarioLiquido;
+        public double aumento;
+        public double valorAumento;
+
+        public double CalcularSalarioLiquido()
+        {
+            salarioLiquido = salarioBruto - (salarioBruto * (imposto / 100));
+            return salarioBruto;
+        }
+
+        public void AumentarSalario()
+        {
+            valorAumento = salarioBruto * (aumento / 100);
+            salarioBruto += valorAumento;
+        }
+    }
+}
